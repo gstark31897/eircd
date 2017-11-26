8dd4f5775edc8563ddd030265e0b52a58ebaaa77
Gavin Stark <gstark31897@gmail.com>
2017-11-26 13:43:05 -0600

    making messages easier to send

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from select import select
 import ssl
 
 
+clients = []
+
+def send_all(message, exclude):
+    for client in clients:
+        if client in exclude:
+            continue
+        client.send(message)
+
+
 class Client:
     def __init__(self, sock, addr):
         self.sock = sock
@@ -17,8 +26,19 @@ class Client:
     def close(self):
         self.sock.close()
 
-    def send(self, message):
+    def send(self, args):
+        message = ""
+        for i in range(0, len(args) - 1):
+            message += "{} ".format(args[i])
+        if len(args) > 1 and " " in args[-1]:
+            message += ":{}\n".format(args[-1])
+        else:
+            message += "{}\n".format(args[-1])
+        print(message)
         self.sock.send(message.encode("utf-8"))
+
+    def send_all(self, message):
+        send_all(message, self)
 
     def _split_message(self, message):
         args = []
@@ -35,25 +55,52 @@ class Client:
             message = self.sock.recv(4096).decode("utf-8")
             if len(message) is 0:
                 return False
+            self.buff += message
+            while "\n" in self.buff:
+                message, self.buff = self.buff.split('\n', 1)
+                args = self._split_message(message)
+                method = "_handle_{}".format(args[0]).lower()
+                print(method)
+                if not getattr(self, method)(*args[1:]):
+                    return False
+            return True
         except:
             return False
-        self.buff += message
-        while "\n" in self.buff:
-            message, self.buff = self.buff.split('\n', 1)
-            print(self._split_message(message))
-#            self.send("I got: " + message)
-        return True
 
     def startup(self):
-        self.send("PING\n")
-        self.send("NOTICE :welcome to the eirc protocol\n")
+        self.send(["PING"])
+        self.send(["NOTICE", "welcome to the eirc protocol"])
+
+    def _handle_cap(self, *args):
+        return True
+
+    def _handle_nick(self, *args):
+        print(args)
+        return True
+
+    def _handle_user(self, *args):
+        print(args)
+        return True
+
+    def _handle_ping(self, *args):
+        self.send(["PONG"])
+        return True
+
+    def _handle_pong(self, *args):
+        return True
+
+    def _handle_quit(self, *args):
+        return False
+
+    def _handle_privmsg(self, channel, message, *args):
+        return True
+        
 
 server = socket(AF_INET, SOCK_STREAM)
 server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 server.bind(("127.0.0.1", 6667))
 server.listen(5)
 
-clients = []
 while True:
     read, write, error = select(clients + [server], [], clients)
     for item in read:
